967b7864a3e70e3555fc75e5a734edb02888e9b5
JustinAaron16 <fjustinaaron@gmail.com>
2025-09-16 21:45:58 +0800

    assignment done

---
+++ HW1.py
@@ -0,0 +1,19 @@
+
+while(True):
+    str=input("Enter dollar($)(* to exit): ")
+
+    if str=="*":
+        print("Bye")
+        break;
+    
+    dollar = [float(x) for x in str.split("@")]
+
+    def convert(value):
+        return(value, value*88.09, value*0.73, value* 7.12) 
+            
+    print("Dollar($)    Indian Ruppe(R)     British(Pound)      China(Y)")
+    
+    for x in dollar:
+        usd, rpe, pnd, yn = convert(x)
+        print(f"{usd}      {rpe}      {pnd}      {yn}")
+        print()

560e9fea3bc47eae263da248a498ca36fcc54520
JustinAaron16 <fjustinaaron@gmail.com>
2025-09-16 21:53:25 +0800

    assignment complete

--- HW1.py
+++ HW1.py
@@ -11,9 +11,9 @@ while(True):
     def convert(value):
         return(value, value*88.09, value*0.73, value* 7.12) 
             
-    print("Dollar($)    Indian Ruppe(R)     British(Pound)      China(Y)")
-    
+    print("Dollar($)    Indian Rupee(R)     British(Pound)      China(Y)")
+
     for x in dollar:
         usd, rpe, pnd, yn = convert(x)
-        print(f"{usd}      {rpe}      {pnd}      {yn}")
+        print(f"{usd}            {rpe}               {pnd}               {yn}")
         print()
